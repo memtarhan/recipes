702c0884412cd810c2d79b8bc99d1dc54c7d8665
Mehmet Tarhan <hi@memtarhan.com>
2025-10-03 08:30:52 +0100

    Complete project

---
+++ src/redux/favoritesSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    favoriterecipes: [], // Updated to handle favorite articles
+};
+
+const favoritesSlice = createSlice({
+    name: "favorites",
+    initialState,
+    reducers: {
+        toggleFavorite: (state, action) => {
+            const recipe = action.payload;
+            const exists = state.favoriterecipes.find(item => item.idFood === recipe.idFood);
+            if (exists) {
+                state.favoriterecipes = state.favoriterecipes.filter(item => item.idFood !== recipe.idFood);
+            } else {
+                state.favoriterecipes.push(recipe);
+            }
+        },
+    },
+});
+
+export const { toggleFavorite } = favoritesSlice.actions;
+export default favoritesSlice.reducer;
